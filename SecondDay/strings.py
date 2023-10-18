4fcf083289f9c702b5050ee702d9b0765d046577
singh1203 <singh1203.ss@gmail.com>
2023-10-18 17:22:05 +0000

    Done with second day

---
+++ SecondDay/strings.py
@@ -0,0 +1,11 @@
+print("Today is a good day to learn Python!") 
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings \n')
+print("Hello" + " " + "World\n")
+greeting = "Hello"
+name = "Saurabh"
+print(greeting + " " + name)
+
+name = input("Please enter your name: ")
+print(greeting + " " + name + "!")
