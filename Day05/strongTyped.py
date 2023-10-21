b0a4c1938a4c04eaa10b45169bd5e0c2a0184ec2
singh1203 <singh1203.ss@gmail.com>
2023-10-21 16:46:15 +0000

    Done with Day 5

---
+++ Day05/strongTyped.py
@@ -0,0 +1,6 @@
+age = 22
+name = 'Saurabh'
+print(name + 'is' + age + 'year old!')
+
+#It should produce error since you cannot concatenate a string with a number.
+#Hence the TypeError is raised i.e "TypeError: can only concatenate str (not "int") to str"
