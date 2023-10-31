f680c9b0c3d73d7b410e30027103108377980fc7
singh1203 <singh1203.ss@gmail.com>
2023-10-31 18:05:09 +0000

    Fixed bracket

--- Day07/String.py
+++ Day07/String.py
@@ -6,5 +6,5 @@ print("fri" in today)        # True
 
 age = 24
 print("My age is " + str(age) + " years")  # Here we are coerced the age into string
-print("My age is {0} years".format(age)
+print("My age is {0} years".format(age))
 print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6}, {7}".format(31, "January", "March", "May", "July", "August", "October", "December"))
