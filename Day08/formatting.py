7323a37b23eb5cd64155895626eae9e2e035bfc9
singh1203 <singh1203.ss@gmail.com>
2023-11-09 14:44:20 +0000

    Done with day 9

--- Day08/formatting.py
+++ Day08/formatting.py
@@ -9,8 +9,17 @@ for i in range(1, 13):
 print()
 
 print("Pi is approximately {0:12}".format(22 / 7))
-print("Pi is approximately {0:12f}".format(22 / 7))
-print("Pi is approximately {0:12.50f}".format(22 / 7))
+print("Pi is approximately {0:12f}".format(22 / 7))   
+print("Pi is approximately {0:12.50f}".format(22 / 7))    # It contains a placeholder {0:12.50f} where the value of Pi will be inserted.
+
+# {0}: This is a placeholder for the first argument passed to the format method.
+
+# :12.50f: This is a formatting specification. It specifies how the value should be displayed.
+
+# 12: Minimum width of the field. The whole number, decimal point, and fractional part together should take up at least 12 characters.
+
+# .50f: This specifies that the value should be formatted as a floating-point number with 50 digits after the decimal point.
+
 print("Pi is approximately {0:52.50f}".format(22 / 7))
 print("Pi is approximately {0:62.50f}".format(22 / 7))
 print("Pi is approximately {0:<72.50f}".format(22 / 7))
---
+++ Day09/f-strings.py
@@ -0,0 +1,4 @@
+age = 24
+name = "John"
+print(f"Hello {name}, you are {age} years old")
+print(f"Pi is approx {22/7:.50f}")Day09/f-strings.py
